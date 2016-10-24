0950bcca5760ba8fcdc02781f610d2a250239965
cam-shotter <cameronshotter@gmail.com>
2016-10-24 21:38:28 +1300

    changed theme, added street performer, balanced points

--- public/main.js
+++ public/main.js
@@ -1,28 +1,41 @@
-var balls = 0;
+var balloons = 0;
 
-function ballJuggle(number){
-    balls = balls + number;
-    document.getElementById("balls").innerHTML = balls;
+function buyBalloon(number){
+    balloons = balloons + number;
+    document.getElementById("balloons").innerHTML = balloons;
 };
 
 var clowns = 0;
 
-function buyCursor(){
-    var cursorCost = Math.floor(10 * Math.pow(1.1,clowns));     //works out the cost of this cursor
-    if(balls >= cursorCost){                                   //checks that the player can afford the cursor
+function buyClown(){
+    var clownCost = Math.floor(10 * Math.pow(1.1,clowns));     //works out the cost of this cursor
+    if(balloons >= clownCost){                                   //checks that the player can afford the cursor
         clowns = clowns + 1;                                   //increases number of clowns
-    	balls = balls - cursorCost;                          //removes the balls spent
+    	balloons = balloons - clownCost;                          //removes the balloons spent
         document.getElementById('clowns').innerHTML = clowns;  //updates the number of clowns for the user
-        document.getElementById('balls').innerHTML = balls;  //updates the number of balls for the user
+        document.getElementById('balloons').innerHTML = balloons;  //updates the number of balloons for the user
     };
     var nextCost = Math.floor(10 * Math.pow(1.1,clowns));       //works out the cost of the next cursor
-    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    document.getElementById('clownCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
+var streetPerformers = 0;
+
+function buyStreetPerformer(){
+    var streetPerformerCost = Math.floor(100 * Math.pow(1.1,streetPerformers));     //works out the cost of this cursor
+    if(balloons >= streetPerformerCost){                                   //checks that the player can afford the cursor
+        streetPerformers = streetPerformers + 1;                                   //increases number of streetPerformers
+    	balloons = balloons - streetPerformerCost;                          //removes the balloons spent
+        document.getElementById('streetPerformers').innerHTML = streetPerformers;  //updates the number of streetPerformers for the user
+        document.getElementById('balloons').innerHTML = balloons;  //updates the number of balloons for the user
+    };
+    var nextCost = Math.floor(100 * Math.pow(1.1,streetPerformers));       //works out the cost of the next cursor
+    document.getElementById('streetPerformerCost').innerHTML = nextCost;  //updates the cursor cost for the user
+};
 
 
 window.setInterval(function(){
 
-	ballJuggle(clowns);
+	buyBalloon(clowns + (5 * streetPerformers));
 
 }, 1000);
